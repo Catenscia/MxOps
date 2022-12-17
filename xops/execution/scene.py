8464d0fe47adcf049904805ffc9de7f245ce0230
Etienne Wallet <et.wallet@gmail.com>
2022-12-17 09:04:09 +0100

    add scene module

---
+++ xops/execution/scene.py
@@ -0,0 +1,91 @@
+"""
+author: Etienne Wallet
+
+This module contains the functions to execute a scene in a scenario
+"""
+from dataclasses import dataclass
+import os
+from pathlib import Path
+import re
+from typing import Dict, List
+from xops.config.config import Config
+from xops.data.data import ScenarioData
+
+from xops.execution import steps
+import yaml
+
+
+@dataclass
+class Scene:
+    allowed_networks: List[str]
+    allowed_scenario: List[str]
+    steps: List[steps.Step]
+
+    def __post_init__(self):
+        """
+        After the initialisation of an instance, if the inner steps are
+        found to be Dict, will try to convert them to Steps instances.
+        Usefull for easy loading from yaml files
+        """
+        if len(self.steps) and isinstance(self.steps[0], Dict):
+            self.steps = steps.instanciate_steps(self.steps)
+
+
+def load_scene(path: Path) -> Scene:
+    """
+    Load a scene file and convert its content into a list
+
+    :param path: _description_
+    :type path: Path
+    :return: _description_
+    :rtype: List[steps.Step]
+    """
+    with open(path.as_posix(), 'r', encoding='utf-8') as file:
+        raw_scene = yaml.safe_load(file)
+
+    return Scene(**raw_scene)
+
+
+def execute_scene(scene_path: Path):
+    """
+    Load and execute a scene
+
+    :param scene_path: path to the scene file
+    :type scene_path: Path
+    """
+    scene = load_scene(scene_path)
+    scenario_data = ScenarioData.get()
+
+    config = Config.get_config()
+    network = config.get_network()
+
+    # check network authorization
+    if network.name not in scene.allowed_networks:
+        raise ValueError(('Scene not allowed to be executed '
+                          f'in the network {network.name}'))
+
+    # check scenario authorizations
+    match_found = False
+    for scenario_pattern in scene.allowed_scenario:
+        if re.match(scenario_pattern, scenario_data.name) is not None:
+            match_found = True
+            break
+    if not match_found:
+        raise ValueError((f'Scene {scene_path} not allowed to be executed '
+                          f'in the scenario {scenario_data.name}'))
+
+    # execute steps
+    for step in scene.steps:
+        step.execute()
+
+
+def execute_directory(directory_path: Path):
+    """
+    Load and execute scenes from a directory
+
+    :param directory_path: path to the directory containing the files
+    :type directory_path: Path
+    """
+    files = sorted(os.listdir(directory_path.as_posix()))
+    for file in files:
+        execute_scene(file)
