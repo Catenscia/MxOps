9028a0d49edc81fc8a7f8e95bf6a330f42f1bba3
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2023-01-31 13:39:02 +0100

    add OnChainTransfer class

--- mxops/execution/msc.py
+++ mxops/execution/msc.py
@@ -14,3 +14,14 @@ class EsdtTransfer:
     token_identifier: str
     amount: int
     nonce: int = 0
+
+
+@dataclass
+class OnChainTransfer:
+    """
+    Represent any type of token transfer on chain
+    """
+    sender: str
+    receiver: str
+    token: str
+    amount: str
