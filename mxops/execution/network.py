250d0efb483576387e06a3c62625559d1845b10b
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2023-01-31 13:39:37 +0100

    add functions to extract transfers from OnChainTransaction

--- mxops/execution/network.py
+++ mxops/execution/network.py
@@ -3,12 +3,17 @@ author: Etienne Wallet
 
 This module contains the functions to pass transactions to the proxy and to monitor them
 """
+from typing import List
+
 from multiversx_sdk_cli.transactions import Transaction
+from multiversx_sdk_cli.accounts import Address
 from multiversx_sdk_network_providers import ProxyNetworkProvider
-from multiversx_sdk_network_providers.transactions import TransactionOnNetwork
+from multiversx_sdk_network_providers.transactions import TransactionOnNetwork, TransactionLogs
+from multiversx_sdk_network_providers.transaction_events import TransactionEvent
 
 from mxops.config.config import Config
 from mxops import errors
+from mxops.execution.msc import OnChainTransfer
 
 
 def send(tx: Transaction) -> str:
@@ -67,3 +72,71 @@ def raise_on_errors(on_chain_tx: TransactionOnNetwork):
         raise errors.InternalVmExecutionError(on_chain_tx)
     if 'signalError' in event_identifiers:
         raise errors.TransactionExecutionError(on_chain_tx)
+
+
+def get_transfer_from_event(event: TransactionEvent):
+    """_summary_
+
+    :param event: _description_
+    :type event: TransactionEvent
+    """
+    if 'Transfer' not in event.identifier:
+        return None
+    token, nonce, amount, receiver = [t.raw for t in event.topics]
+    token = token.decode()
+    nonce = nonce.hex()
+    if nonce != '':
+        token += '-' + nonce
+    receiver = Address(receiver.hex()).bech32()
+    amount = str(int(amount.hex(), 16))
+    return OnChainTransfer(event.address.bech32(), receiver, token, amount)
+
+
+def get_transfers_from_logs(on_chain_logs: TransactionLogs) -> List[OnChainTransfer]:
+    """_summary_
+
+    :param on_chain_logs: _description_
+    :type on_chain_logs: TransactionLogs
+    :return: _description_
+    :rtype: List[OnChainTransfer]
+    """
+    transfers = []
+    for event in on_chain_logs.events:
+        transfer = get_transfer_from_event(event)
+        if transfer is not None:
+            transfers.append(transfer)
+    return transfers
+
+
+def get_on_chain_transfers(on_chain_tx: TransactionOnNetwork) -> List[OnChainTransfer]:
+    """
+    Extract from an on-chain transaction the tokens transfers that were operated in this
+    transaction.
+
+    :param on_chain_tx: on-chain transaction to inspect
+    :type on_chain_tx: TransactionOnNetwork
+    :return: list of executed transfer
+    :rtype: List[OnChainTransfer]
+    """
+    transfers = []
+    if on_chain_tx.value != "0":
+        transfers.append(OnChainTransfer(
+            on_chain_tx.sender.bech32(),
+            on_chain_tx.receiver.bech32(),
+            'EGLD',
+            on_chain_tx.value
+        ))
+
+    for result in on_chain_tx.contract_results.items:
+        sender, receiver = result.sender.bech32(), result.receiver.bech32()
+        if result.value != 0 and sender != receiver:
+            transfers.append(OnChainTransfer(
+                result.sender,
+                result.receiver,
+                'EGLD',
+                result.value
+            ))
+        transfers.extend(get_transfers_from_logs(result.logs))
+
+    transfers.extend(get_transfers_from_logs(on_chain_tx.logs))
+    return transfers
