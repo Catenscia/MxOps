bec1cd78dad3ad5ecaab46217f15ce210b08ace7
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2023-05-04 18:15:29 +0200

    tests: adapt token management tests to the new xnetwork setup

--- integration_tests/token_management/scripts/run_test.sh
+++ integration_tests/token_management/scripts/run_test.sh
@@ -18,4 +18,5 @@ python -m mxops \
             execute \
             -n $1 \
             -s integration_test_token_management \
+            integration_tests/setup_scenes/01_accounts.yaml \
             integration_tests/token_management/mxops_scenes
