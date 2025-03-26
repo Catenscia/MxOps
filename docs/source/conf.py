5d2e3476895c06915addcbdb8b9ee9c0d777f4d3
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2025-03-26 14:36:04 +0100

    docs: add sphinx material and sphinx design

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,7 @@ version = version_string
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "myst_parser",
-]
+extensions = ["myst_parser", "sphinx_material", "sphinx_design"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
