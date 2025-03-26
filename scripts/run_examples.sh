29d3856dfb1a1dd46bba1f7875779c0a0d73b125
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2025-03-26 15:00:38 +0100

    refactor: rename tutorials folder to examples

--- scripts/run_examples.sh
+++ scripts/run_examples.sh
@@ -2,19 +2,19 @@
 set -e
 
 # create a new env and activate it
-ENV_NAME=".mxops_tutorial_env"
+ENV_NAME=".mxops_examples_env"
 uv venv $ENV_NAME --python 3.11
 source ${ENV_NAME}/bin/activate
 
 # install project
 uv pip install .
 
-# launch first scene tutorial
-cd tutorials/first_scene
+# launch first scene example
+cd examples/first_scene
 bash scripts/build_contract.sh
 bash scripts/execute_first_scene.sh
 
-# launch enhanced first scene tutorial
+# launch enhanced first scene example
 cd ../enhanced_first_scene
 bash scripts/build_contract.sh
 bash scripts/execute_deploy_scene.sh
