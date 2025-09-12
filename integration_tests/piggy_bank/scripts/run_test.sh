8382b7f2934fd6bc704c4040a1e233bfbd370674
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2025-09-12 17:37:53 +0200

    tests: add integration tests for log and fail checks

--- integration_tests/piggy_bank/scripts/run_test.sh
+++ integration_tests/piggy_bank/scripts/run_test.sh
@@ -7,4 +7,5 @@ mxops \
     -s integration_test_piggy_bank_user_exploit \
     -c \
     integration_tests/setup_scenes/01_accounts.yaml \
-    integration_tests/piggy_bank/mxops_scenes/user_exploit
+    integration_tests/piggy_bank/mxops_scenes/user_exploit \
+    integration_tests/piggy_bank/mxops_scenes/endpoint_verification
